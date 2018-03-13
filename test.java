58db0a588df7691be0638af1d9492b04f59ad99e
Kartik Arora <33497301+ktech99@users.noreply.github.com>
2018-03-13 12:34:20 -0700

    Initial Commit
    Change the code with code you wrote, and run to test.

---
+++ test.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class test {
+  public static void main(String args[]) {
+    // For initialising your 2D array
+    String[][] values = {{"y", "a", "y"}, {"b", "r", "e", "a", "k"}};
+    System.out.println(mystery(values));
+  }
+
+  // Replace with method you wrote
+  // change return type
+  // change parameters
+  public static /*Map<String, Integer>*/ mystery(String[][] a) {}
+}
